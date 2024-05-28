0f8b037135a541fd4498e77c73b3caab4975961d
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-28 16:21:57 +0200

    Adjust routes indent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { ContactComponent } from './pages/contact/contact.component';
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path:'about', component: AboutComponent},
+    { path: 'about', component: AboutComponent},
     { path: 'skills', component: SkillsComponent },
     { path: 'projects', component: ProjectsComponent },
     { path: 'contact', component: ContactComponent },
