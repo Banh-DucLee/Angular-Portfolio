0c0951cb1f986bd2333d4b136c65b06a72b771cc
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-31 14:17:20 +0200

    Created SliderHomeComponent with Animation

---
+++ src/app/parts/slider/slider-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slider-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './slider-home.component.html',
+  styleUrl: './slider-home.component.scss'
+})
+export class SliderHomeComponent {
+
+}
