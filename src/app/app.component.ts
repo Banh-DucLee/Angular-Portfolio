1e4269c973aebd9e63b67912e30e17dacc9d4fea
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-31 14:13:38 +0200

    Add imported components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './parts/header/header.component';
+import { NavIndicatorsComponent } from './parts/nav-indicators/nav-indicators.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavIndicatorsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
