97a0878569e0a8dc2539f599852fe36af860fd42
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-31 15:30:51 +0200

    Added main HTML structure to AboutPage

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { LanguageService } from '../../services/language/language.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit, OnDestroy {
+  languageService: LanguageService = inject(LanguageService);
 
+  private lnaguageSubcription!: Subscription;
+  titleText: string = '';
+  descriptionText: string = '';
+
+  ngOnInit(): void {
+      this.lnaguageSubcription = this.languageService.language$.subscribe((language) => {
+        this.titleText = language == 'fr' ? `À Propos` : `About`;
+        this.descriptionText = language == 'fr' ? `Passionné d'informatique depuis mon plus jeune âge, j'ai appris la programmation en autodidacte avant de suivre une formation pour devenir développeur web. J'aime concrétiser mes idées en écrivant un code élégant et efficace` : `Passionate about computer science from a young age, I taught myself programming before undertaking training to become a web developer. I enjoy bringing my ideas to life by writing elegant and efficient code`;
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.lnaguageSubcription.unsubscribe();
+  }
 }
