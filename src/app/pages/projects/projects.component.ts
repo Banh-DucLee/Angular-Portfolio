698cf4c016c018ab90e0dfed8affac49463bd9e2
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-31 22:21:18 +0200

    Created ProjectPage main HTML structure

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageService } from '../../services/language/language.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-projects',
@@ -8,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  languageService: LanguageService = inject(LanguageService);
+
+  private lnaguageSubcription!: Subscription;
+  titleText: string = '';
+
+  ngOnInit(): void {
+      this.lnaguageSubcription = this.languageService.language$.subscribe((language) => {
+        this.titleText = language == 'fr' ? `Mes Projets` : `My Projects`;
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.lnaguageSubcription.unsubscribe();
+  }
 
 }
