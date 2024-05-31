a94f5796dd6bcee6a39f96c8b85cfea389d7c7e8
Duc-Lee Banh <duclee.banh@gmail.com>
2024-05-31 14:15:47 +0200

    Fully functionnal ToggleButtonComponent

---
+++ src/app/components/toggle-button/toggle-button.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-toggle-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './toggle-button.component.html',
+  styleUrl: './toggle-button.component.scss'
+})
+export class ToggleButtonComponent {
+@Input() Icon: string = '';
+
+@Output() onClick = new EventEmitter<void>();
+
+handleClick(): void {
+  this.onClick.emit();
+}
+}
