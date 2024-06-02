357fd7cace8c0abfaa79774acd705e4eb41842e4
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-03 01:24:14 +0200

    Added Logout button to header when Login

--- src/app/parts/header/header.component.ts
+++ src/app/parts/header/header.component.ts
@@ -3,29 +3,39 @@ import { LanguageService } from '../../services/language/language.service';
 import { Subscription } from 'rxjs';
 import { ToggleButtonComponent } from '../../components/toggle-button/toggle-button.component';
 import { NavButtonComponent } from '../nav-button/nav-button.component';
-
+import { FetchAPIService } from '../../services/fetch/fetch-api.service';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { matLogoutRound } from '@ng-icons/material-icons/round'
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ToggleButtonComponent, NavButtonComponent],
+  imports: [ToggleButtonComponent, NavButtonComponent, NgIconComponent],
+  viewProviders: [provideIcons({matLogoutRound})],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   languageService: LanguageService = inject(LanguageService);
+  fetchAPIService: FetchAPIService = inject(FetchAPIService);
 
   private languageSubscription!: Subscription;
   languageButtonIcon: string = '';
 
+  private loginSubscription!: Subscription;
+  isLogin: boolean = false;
 
   ngOnInit(): void {
     this.languageSubscription = this.languageService.language$.subscribe((language) => {
       this.languageButtonIcon = language.toUpperCase();
-    })
+    });
+    this.loginSubscription = this.fetchAPIService.isLogin$.subscribe((log) => {
+      this.isLogin = log;
+    });
   }
 
   ngOnDestroy(): void {
     this.languageSubscription.unsubscribe();
+    this.loginSubscription.unsubscribe();
   }
 
 }
