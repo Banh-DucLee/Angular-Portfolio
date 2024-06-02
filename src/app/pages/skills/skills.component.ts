c7975b82c317f34dfc0289ca7f8c3079021e214c
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-03 00:22:27 +0200

    Added modify wutton when login

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { LanguageService } from '../../services/language/language.service';
 import { Subscription } from 'rxjs';
+import { FetchAPIService } from '../../services/fetch/fetch-api.service';
 
 @Component({
   selector: 'app-skills',
@@ -11,17 +12,28 @@ import { Subscription } from 'rxjs';
 })
 export class SkillsComponent implements OnInit, OnDestroy {
   languageService: LanguageService = inject(LanguageService);
+  fetchAPIService: FetchAPIService = inject(FetchAPIService);
 
-  private lnaguageSubcription!: Subscription;
+  private languageSubcription!: Subscription;  
   titleText: string = '';
 
+  private loginSubscription!: Subscription;
+  isLogin: boolean = false;
+
   ngOnInit(): void {
-      this.lnaguageSubcription = this.languageService.language$.subscribe((language) => {
+      this.languageSubcription = this.languageService.language$.subscribe((language) => {
         this.titleText = language == 'fr' ? `Mes Compétences` : `My Skills`;
       })
+      this.loginSubscription = this.fetchAPIService.isLogin$.subscribe((log) => {
+        this.isLogin = log;
+      })
+      this.fetchAPIService.getSkills();
   }
 
   ngOnDestroy(): void {
-      this.lnaguageSubcription.unsubscribe();
+      this.languageSubcription.unsubscribe();
+      this.loginSubscription.unsubscribe();
   }
+
+  modify() {}
 }
