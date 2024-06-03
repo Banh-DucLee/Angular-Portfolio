ccaf553e9c7486e401c89286344ee5e6eaed1c63
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-03 12:47:48 +0200

    Created modal component

---
+++ src/app/components/modal/modal.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { matCloseRound } from '@ng-icons/material-icons/round';
+import { ModalStatusService } from '../../services/modal/modal-status.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({ matCloseRound })],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss'
+})
+export class ModalComponent implements OnInit, OnDestroy {
+  modalStatusService: ModalStatusService = inject(ModalStatusService);
+
+  private modalSubscription!: Subscription;
+  isModalOpen: boolean = false;
+
+  ngOnInit(): void {
+      this.modalSubscription = this.modalStatusService.isOpen$.subscribe((modalStatus) => {
+        this.isModalOpen = modalStatus;
+      })
+  }
+
+  ngOnDestroy(): void {
+      this.modalSubscription.unsubscribe();
+  }
+}
