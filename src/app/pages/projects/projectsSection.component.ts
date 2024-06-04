bb2e14c91a2917081c70ff40d32fc2e5bc4dfcac
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-04 18:46:43 +0200

    Added Modal to modify and ProjectPageComponent

--- src/app/pages/projects/projectsSection.component.ts
+++ src/app/pages/projects/projectsSection.component.ts
@@ -2,23 +2,46 @@ import { Component, inject } from '@angular/core';
 import { LanguageService } from '../../services/language/language.service';
 import { Subscription } from 'rxjs';
 import { FetchAPIService } from '../../services/fetch/fetch-api.service';
+import { Project } from '../../interfaces/Project.interface';
+import { ProjectComponent } from '../../components/project/project.component';
+import { ModalStatusService } from '../../services/modal/modal-status.service';
+import { ModalComponent } from '../../components/modal/modal.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Skill } from '../../interfaces/Skill.inteface';
 
 @Component({
   selector: 'app-projects-section',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent, ModalComponent, ReactiveFormsModule],
   templateUrl: './projectsSection.component.html',
   styleUrl: './projectsSection.component.scss'
 })
 export class ProjectsSectionComponent {
   languageService: LanguageService = inject(LanguageService);
   fetchAPIService: FetchAPIService = inject(FetchAPIService);
+  modalStatusService: ModalStatusService = inject(ModalStatusService);
 
   private languageSubcription!: Subscription;
   titleText: string = '';
 
   private loginSubscription!: Subscription;
   isLogin: boolean = false;
+
+  private modalSubscription!: Subscription;
+  isModalOpen: boolean = false;
+
+  projects: Project[] = [];
+  skills: Skill[] = [];
+  selectedSkills: string[] = [];
+
+  projectForm = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    description: new FormControl('', [Validators.required]),
+    descriptionEnglish: new FormControl('' ,[Validators.required]),
+    githubUrl: new FormControl(''),
+    liveDemoUrl: new FormControl('')
+  })
+  selectedFile: File | null = null;
 
   ngOnInit(): void {
       this.languageSubcription = this.languageService.language$.subscribe((language) => {
@@ -27,15 +50,61 @@ export class ProjectsSectionComponent {
       this.loginSubscription = this.fetchAPIService.isLogin$.subscribe((log) => {
         this.isLogin = log;
       })
+      this.modalSubscription = this.modalStatusService.isOpen$.subscribe((modalStatus) => {
+        this.isModalOpen = modalStatus;
+      })
+      this.projects = this.fetchAPIService.getProjects();
+      this.skills = this.fetchAPIService.getSkills();
   }
 
   ngOnDestroy(): void {
       this.languageSubcription.unsubscribe();
       this.loginSubscription.unsubscribe();
+      this.modalSubscription.unsubscribe();
   }
 
   modify() {
-    
+    this.modalStatusService.toggleOpen();
+  }
+
+  addSkill(skillId: string): void {
+    if (skillId && !this.selectedSkills.includes(skillId)) {
+      this.selectedSkills.push(skillId);
+    }
+  }
+
+  removeSkill(id: string): void {
+    this.selectedSkills = this.selectedSkills.filter( obj => {
+      return obj !== id;
+    });
+  }
+
+  getSkillName(id: string): string {
+    const name = this.skills.find( s => s._id === id)?.name!;
+    return name;
+  }
+
+  onSubmit(event: Event) {
+    let project: Project = {
+      _id: '',
+      title: this.projectForm.get('title')?.value!,
+      description: this.projectForm.get('description')?.value!,
+      descriptionEnglish: this.projectForm.get('descriptionEnglish')?.value!,
+      githubUrl: this.projectForm.get('githubUrl')?.value!,
+      liveDemoUrl: this.projectForm.get('liveDemoUrl')?.value!,
+      skills: this.selectedSkills,
+      imageUrl: ''
+    }
+    this.fetchAPIService.createProject(project, this.selectedFile!);
+    this.projectForm.reset();
+    this.selectedSkills = [];
+  }
+
+  onFileChange(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      this.selectedFile = file;
+    }
   }
 
 }
