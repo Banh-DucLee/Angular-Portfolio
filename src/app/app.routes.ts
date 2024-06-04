608cddecb6745ffa10772e29df87090a2e775691
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-04 18:40:26 +0200

    Added project:id routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { HomePageComponent } from './pages/home/homePage.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProjectPageComponent } from './pages/project-page/project-page.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'project/:id', component: ProjectPageComponent },
     { path: '**', component: PageNotFoundComponent }
 ];
