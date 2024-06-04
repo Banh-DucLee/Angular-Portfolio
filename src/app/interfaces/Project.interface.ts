5c60dee8fa3e09ee873f9d012ab7b52f0ff73043
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-04 18:47:38 +0200

    Created Project interface for API request

---
+++ src/app/interfaces/Project.interface.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    _id: string;
+    title: string;
+    imageUrl: string;
+    description: string;
+    descriptionEnglish: string;
+    githubUrl: string;
+    liveDemoUrl: string;
+    skills: string[];
+}
