7347a5578f1f817ce29e7bb091b5be6b221ceee6
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-14 00:36:46 +0200

    Removed unused components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './parts/header/header.component';
-import { NavIndicatorsComponent } from './parts/nav-indicators/nav-indicators.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavIndicatorsComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
