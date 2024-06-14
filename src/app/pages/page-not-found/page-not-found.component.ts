c04b019ac5ed0733e85537f50393e669eb592b60
Duc-Lee Banh <duclee.banh@gmail.com>
2024-06-14 13:38:23 +0200

    Fully functionnal and responsive error page

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { LanguageService } from '../../services/language/language.service';
+import { Subscription } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit, OnDestroy{
+  languageService: LanguageService = inject(LanguageService);
 
+  private languageSubcription!: Subscription;
+  descriptionText: string = '';
+  buttonText: string = '';
+  
+
+  ngOnInit(): void {
+    this.languageSubcription = this.languageService.language$.subscribe((language) => {
+      this.descriptionText = language == 'fr' ? 'La page que vous cherchez n\'existe pas' : 'The page you are looking for does not exist';
+      this.buttonText = language == 'fr' ? 'Retour à la page l\'accueil' : 'Back to the main page';
+    })
+  }
+
+  ngOnDestroy(): void {
+      this.languageSubcription.unsubscribe();
+  }
 }
